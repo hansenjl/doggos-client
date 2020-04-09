ddf21bc6e1c625cb763ea2ed84d59f6e4d47b6dd
hansenjl <jenn.leigh.hansen@gmail.com>
2020-04-08 21:26:07 -0700

    remove likes display

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -4,8 +4,6 @@ function Dog(props){
     return(
         <div className="centering">
             <div className="dog-image">
-                { props.likes ? <h3>Likes: {props.likes}</h3> : null}
-
             <img src={`http://random.dog/${props.url}`}></img>
             </div>
         </div>
