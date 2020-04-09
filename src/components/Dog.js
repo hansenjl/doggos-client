9f70c72d9b0b81c8e5008a92c010dcb320e3001e
hansenjl <jenn.leigh.hansen@gmail.com>
2020-04-08 22:25:35 -0700

    add fav and unfav functions

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addDog, removeDog} from '../actions/dogActions'
 
 function Dog(props){
+
+    const unFav = (e) => {
+
+        let dog = {
+            id: props.id,
+            url: props.url
+        }
+        
+        props.removeDog(dog)
+    }
+
+    const addFav = (e) => {
+        let dog = {url: props.url}
+        props.addDog(dog)
+    }
+
+
+
     return(
         <div className="centering">
             <div className="dog-image">
-            <img src={`http://random.dog/${props.url}`}></img>
+                <img onClick={props.fav ? unFav : addFav} id={`dog-${props.id}`} src={`http://random.dog/${props.url}`} alt="cute doggo"></img>
             </div>
         </div>
     )
@@ -12,4 +32,4 @@ function Dog(props){
 }
 
 
-export default Dog
+export default connect(null, {addDog, removeDog})(Dog)
