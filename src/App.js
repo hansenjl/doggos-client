b72c99951e9ca153789ceffce8c543f9d145a3aa
hansenjl <jenn.leigh.hansen@gmail.com>
2020-04-08 21:16:08 -0700

    move fetch all dogs call to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {  Switch, Link, Route } from 'react-router-dom'
+import DogsContainer from './containers/DogsContainer'
+import FavDogsContainer from './containers/FavDogsContainer'
+import {getAllDogs} from "./actions/dogActions"
+import {connect} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  componentDidMount(){
+    this.props.getAllDogs()
+    //fetch dogs and set laoding as true while fetching dogs
+  }
+
+  render(){
+    return (
+
+        <div className="App">
+          <header className="App-header">
+            <p>
+              <button><Link to="/faves">Favorite Doggos</Link></button>
+              <button><Link to="/">All Doggos</Link></button>
+            </p>
+          </header>
+          <Switch>
+            <Route exact path="/faves"><FavDogsContainer/></Route>
+            <Route exact path="/"><DogsContainer/></Route>
+          </Switch>
+
+        </div>
+
+    );
+  }
 }
 
-export default App;
+
+
+
+
+export default connect(null,{getAllDogs})(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import { createStore , applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {BrowserRouter as Router} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <Provider store={store}>
+       <App />
+    </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
