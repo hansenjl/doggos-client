a7c297d88b7075ea159410a5ec027844b832381f
hansenjl <jenn.leigh.hansen@gmail.com>
2020-04-08 22:26:25 -0700

    add cases for adding and deleting dogs

--- src/reducers/dogReducer.js
+++ src/reducers/dogReducer.js
@@ -15,6 +15,21 @@ const dogReducer = (state = {dogs:[], favorites: [], loading: false}, action) =>
                 dogs: action.payload,
                 loading: false
             }
+        case 'ADDED_DOG':
+            let idx = state.dogs.findIndex((dog) => {return action.payload.url === dog.url})
+
+            return{
+                ...state,
+                favorites: state.favorites.concat(action.payload),
+                dogs: [...state.dogs.slice(0,idx), ...state.dogs.slice(idx+1, state.dogs.length+1)]
+            }
+        case 'REMOVED_DOG':
+            let id = state.favorites.findIndex((dog) => {return action.payload.id === dog.id})
+            return {
+                ...state,
+                dogs: state.dogs.concat(action.payload),
+                favorites: [...state.favorites.slice(0,id), ...state.favorites.slice(id+1, state.favorites.length+1)]
+            }
         default:
             return state
     }
